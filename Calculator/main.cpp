87e039a2c9480a694cc37cc601055613aa1ee727
Bradart <20838090+ArtBradart@users.noreply.github.com>
2022-05-06 16:13:55 +0700

    Colorize

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
+
 #include "Expression.h"
 
 using namespace std;
 
-string Calculate(const string& source)
+
+struct Result {
+	string str = "";
+	bool isValid = true;
+};
+
+enum class EColor : WORD {
+	Title = 0x0003,
+	Input = 0x000F,
+	Result = 0x0002,
+	Error = 0x0004
+};
+
+
+void SetColor(EColor color)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, (WORD)color);
+}
+
+Result Calculate(const string& source)
 {
 	Expression exp(source);
-	if (!exp.IsValid())
-	{
-		return exp.GetErrorMsg();
-	}
-	return to_string(exp.GetResult());
+	return exp.IsValid() ? 
+		Result({ to_string(exp.GetResult()), true }):
+		Result({ exp.GetErrorMsg(), false });
 }
+
 
 int main(int argc, char** argv)
 {
+	
 	while (true)
 	{
+		SetColor(EColor::Title);
 		cout << "Input: ";
+
+		SetColor(EColor::Input);
 		string str;
 		getline(cin, str);
-		cout << Calculate(str) << endl << endl;
+
+		Result result = Calculate(str);
+		SetColor(result.isValid ? EColor::Result : EColor::Error);
+		cout << result.str << endl << endl;
 	}
 
 	return 0;
